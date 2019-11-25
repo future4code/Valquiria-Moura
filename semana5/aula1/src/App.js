44978e322032fe4ec81f17b2614cf64720c02e2a
vaugustineli <vaugustineli@gmail.com>
2019-11-25 16:48:49 -0300

    Salvando semana 5 aula 1

---
+++ semana5/aula1/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header >
+        <
+        p > Assisti a aula do Goli novamente durante a tarde, < /p> <
+            p > pausando e testando tudo. < /p> <
+            p > Então me perdi com o horário e não fiz nada do Exercício. < /p> <
+                p > Enfim...Me desculpe... < /p> <
+                /header> <
+                /div>
+    );
+}
+
+export default App;
