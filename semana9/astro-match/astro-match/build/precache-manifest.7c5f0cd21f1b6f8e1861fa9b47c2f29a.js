1b8f7dc6fe7c4a3333b85619a2b6c3f0abbd5aaa
vaugustineli <vaugustineli@gmail.com>
2020-01-09 16:33:56 -0300

    Não consegui nem começar =/

---
+++ semana9/astro-match/astro-match/build/precache-manifest.7c5f0cd21f1b6f8e1861fa9b47c2f29a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b6d116f8cf1dbe606d41ae9c57fc97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50496159529a0c4bf6d1",
+    "url": "/static/js/2.e207dca2.chunk.js"
+  },
+  {
+    "revision": "78bad7657fddb4e149b4",
+    "url": "/static/js/main.5033bebc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
